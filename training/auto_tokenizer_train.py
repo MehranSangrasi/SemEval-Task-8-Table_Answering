c848775a33b08761061922d5bd71b40a34b91dea
Mehran Wahid <mehransangrasi@gmail.com>
2024-12-21 19:58:55 +0500

    notebook

--- training/auto_tokenizer_train.py
+++ training/auto_tokenizer_train.py
@@ -2,7 +2,41 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from trl import setup_chat_format
 
+from datasets import load_dataset
+
+
+torch.device("mps")
  
+# Convert dataset to OAI messages
+system_message = """You are an text to SQL query translator. Users will ask you questions in English and you will generate a SQL query based on the provided SCHEMA.
+SCHEMA:
+{schema}"""
+ 
+def create_conversation(sample):
+  return {
+    "messages": [
+      {"role": "system", "content": system_message.format(schema=sample["context"])},
+      {"role": "user", "content": sample["question"]},
+      {"role": "assistant", "content": sample["answer"]}
+    ]
+  }
+ 
+# Load dataset from the hub
+dataset = load_dataset("b-mc2/sql-create-context", split="train")
+dataset = dataset.shuffle().select(range(12500))
+ 
+# Convert dataset to OAI messages
+dataset = dataset.map(create_conversation, remove_columns=dataset.features,batched=False)
+# split dataset into 10,000 training samples and 2,500 test samples
+dataset = dataset.train_test_split(test_size=2500/12500)
+ 
+print(dataset["train"][345]["messages"])
+ 
+# save datasets to disk
+dataset["train"].to_json("train_dataset.json", orient="records")
+dataset["test"].to_json("test_dataset.json", orient="records")
+
+
 # Hugging Face model id
 model_id = "codellama/CodeLlama-7b-hf" # or `mistralai/Mistral-7B-v0.1`
  
@@ -91,4 +125,3 @@ trainer.save_model()
 # free the memory again
 del model
 del trainer
-torch.cuda.empty_cache()
