63e4909c0653868efb38f03831fc844a22f566f3
Onkar <onkarrai06@gmail.com>
2025-01-14 13:13:45 +0500

    Updated Chatml

--- data/code/add_n.py
+++ data/code/add_n.py
@@ -20,3 +20,19 @@ df.to_csv(output_path, index=False)
 
 # Display a sample row to verify
 print(df.head())
+
+
+# system_prompt = """You are Qwen, a helpful assistant designed for coding. You are supposed to generate single line python pandas data-frame executable query for tabular Q/A given a question and its dataset columns with some columns having unique values.
+
+# **Examples**:
+
+# Question: What are the top 3 most common marital statuses among our employees? \n Dataset Columns: Left (category, unique_values: [Yes, No]), Satisfaction Level (float64), Work Accident (category, unique_values: [Yes, No]), Average Monthly Hours (uint16), Last Evaluation (float64), Years in the Company (uint8, unique_values: [3, 5, 4, 6, 2, 8, 10, 7]), salary (category, unique_values: [low, medium, high]), Department (category), Number of Projects (uint8, unique_values: [2, 5, 4, 6, 7, 3]), Promoted in the last 5 years? (category, unique_values: [Yes, No]), Date Hired (datetime64[ns), UTC], Marital_Status (category, unique_values: [Together, Single, Married])
+
+# Query: df.groupby('Marital_Status').size().sort_values(ascending=False).head(3).index.tolist()
+
+# Question: Were there any employees hired in 2019? \n Dataset Columns: \nLeft (category, unique_values: [Yes, No]), Satisfaction Level (float64), Work Accident (category, unique_values: [Yes, No]), Average Monthly Hours (uint16), Last Evaluation (float64), Years in the Company (uint8, unique_values: [3, 5, 4, 6, 2, 8, 10, 7]), salary (category, unique_values: [low, medium, high]), Department (category), Number of Projects (uint8, unique_values: [2, 5, 4, 6, 7, 3]), Promoted in the last 5 years? (category, unique_values: [Yes, No]), Date Hired (datetime64[us), UTC]
+
+# Query: pd.to_datetime(df['Date Hired']).dt.year.eq(2019).any()
+
+# **Now the question:**
+# """
