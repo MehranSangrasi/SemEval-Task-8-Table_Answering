7fb3a7a432df4f4765236386381a3e83ddc8c78f
Onkar <onkarrai06@gmail.com>
2025-01-14 14:57:47 +0500

    Chatml (Axolotl) - Fixed Errors

--- data/code/alpaca_chatml_2.py
+++ data/code/alpaca_chatml_2.py
@@ -1,24 +1,23 @@
 import json
 
 
-with open('train_dev_test/axolotl/transformed/alpaca_trans_2.json') as f:
+with open('train_dev_test/axolotl/transformed/alpaca_trans.json') as f:
     data = json.load(f)
     
-json_obj = []
+json_obj = {"conversations": []}
     
-conversations = {"conversations":[]}
 for item in data:
-    conversations = {"conversations":[]}
+    conversation = []
     
     conversation_human = {"from": "human", "value": item['instruction']}
     conversation_gpt = {"from": "assistant", "value": item['output']}
     
-    conversations["conversations"].append(conversation_human)
-    conversations["conversations"].append(conversation_gpt)
+    conversation.append(conversation_human)
+    conversation.append(conversation_gpt)
     
-    json_obj.append(conversations)
+    json_obj["conversations"].append(conversation)
     
 print(len(json_obj))
 
-with open('train_dev_test/axolotl/transformed/chatml_trans_2.json', 'w') as f:
+with open('train_dev_test/axolotl/transformed/chatml_trans_3.json', 'w') as f:
     json.dump(json_obj, f, indent=4)
